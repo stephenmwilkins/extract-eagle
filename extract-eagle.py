107725a2d5fd11a9be5f3905cb934b1f56e79ee5
stephenmwilkins <ste.wilkins@gmail.com>
2025-10-03 13:04:42 +0100

    updated

--- extract-eagle.py
+++ extract-eagle.py
@@ -10,7 +10,37 @@ synthesizer_dir = '/cosma7/data/dp004/dc-payy1/my_files/synthesizer/synthesizer-
 subfind_dir = '/cosma7/data/Eagle/ScienceRuns/Planck1/L0100N1504/PE/REFERENCE/data'
 
 
-snapshots = ['008_z005p037']
+snapshots = [
+    '000_z020p000',
+    # '001_z015p132',
+    # '002_z009p993',
+    # '003_z008p988',
+    # '004_z008p075',
+    # '005_z007p050',
+    # '006_z005p971',
+    # '007_z005p487',
+    # '008_z005p037',
+    # '009_z004p485',
+    # '010_z003p984',
+    # '011_z003p528',
+    # '012_z003p017',
+    # '013_z002p478',
+    # '014_z002p237',
+    # '015_z002p012',
+    # '016_z001p737',
+    # '017_z001p487',
+    # '018_z001p259',
+    # '019_z001p004',
+    # '020_z000p865',
+    # '021_z000p736',
+    # '022_z000p615',
+    # '023_z000p503',
+    # '024_z000p366',
+    # '025_z000p271',
+    # '026_z000p183',
+    # '027_z000p101',
+    # '028_z000p000',
+    ]
 
 lines = ['H 1 6562.80A', 'H 1 4861.32A', 'H 1 1.87510m']
 
@@ -23,9 +53,7 @@ for snapshot in snapshots:
 
     # Count only files
     N = sum(1 for f in folder.iterdir() if f.is_file())
-
     print(f"{snapshot} {N}")
-
 
     # get quantities
 
@@ -39,8 +67,8 @@ for snapshot in snapshots:
 
         with h5py.File(filename) as hf:
 
-            bh_mass_list.append(hf['Subhalo/ApertureMeasurements/Mass/030kpc'][:,4])
-            stellar_mass_list.append(hf['Subhalo/ApertureMeasurements/Mass/030kpc'][:,3])
+            bh_mass_list.append(hf['Subhalo/ApertureMeasurements/Mass/030kpc'][:,5])
+            stellar_mass_list.append(hf['Subhalo/ApertureMeasurements/Mass/030kpc'][:,4])
             sfr_list.append(hf['Subhalo/ApertureMeasurements/SFR/030kpc'][:])
 
 
